302cfd5b0f1ed69333434f147f9363a1e5a8315a
Cat Zimmermann <csz@bathtowel.local>
2022-12-14 11:03:31 -0800

    Add exercise 2.1

--- SICP Section 2.1.1.rkt
+++ SICP Section 2.1.1.rkt
@@ -1,12 +1,24 @@
 #lang sicp
+;
+; Section 2.1.1
+;
+
+; gcd defition from Section 1.2.5:
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
 
 ; Debuggable implementatation of ctor and selectors:
 ; (define (make-rat n d) (cons n d))
 ; (define (numer x) (car x))
 ; (define (denom x) (cdr x))
 
-; More efficient, removes a procedure call for each operation:
-(define make-rat cons)
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+; More efficient, removes a procedure call for the selectors
 (define numer car)
 (define denom cdr)
 
@@ -17,3 +29,22 @@
 
 (define one-half (make-rat 1 2))
 (add-rat one-half one-half)
+
+(define one-fourth (make-rat 1 4))
+(add-rat one-fourth one-fourth)
+
+;
+; Exercise 2.1
+;
+(define (xor a b) (if a (not b) b))
+
+(define (make-rat-normalize n d)
+  (let ((g (abs (gcd n d)))
+        (an (abs n))
+        (ad (abs d)))
+    (if (xor (< n 0) (< d 0))
+        (cons (/ (* -1 an) g) (/ ad g))
+        (cons (/ n g) (/ d g)))))
+
+(make-rat-normalize 1 -4)
+(make-rat-normalize -1 4)
