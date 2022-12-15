453d0286cfae14136a8c204643edd936e8b7dec4
Cat Zimmermann <csz@bathtowel.local>
2022-12-15 10:12:44 -0800

    Implement exercise 2.3

--- SICP Section 2.1.2.rkt
+++ SICP Section 2.1.2.rkt
@@ -19,3 +19,32 @@
 
 (midpoint-segment (make-segment (make-point -1 -1) (make-point 1 1)))
 (midpoint-segment (make-segment (make-point 0 0) (make-point 10 10)))
+
+;
+; Exercise 2.3
+;
+; (define (make-rectangle lower-left upper-right))
+; @TODO: in ctor, assert order of arguments or normalize internally
+(define make-rectangle cons)
+(define lower-left car)
+(define upper-right cdr)
+
+(define r (make-rectangle (make-point 0 0) (make-point 10 10)))
+
+(define (width rect)
+  (define llx (x-point (lower-left rect)))
+  (define urx (x-point (upper-right rect)))
+  (- urx llx))
+
+(define (height rect)
+  (define lly (y-point (lower-left rect)))
+  (define ury (y-point (upper-right rect)))
+  (- ury lly))
+
+(width r)
+(height r)
+
+(define (area rect)
+  (* (width rect) (height rect)))
+
+(area r)
